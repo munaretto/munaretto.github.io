bafa90f0c2a5d2b1ef81b0915cfafd9cb3cadc5d
Guilherme Munaretto <munaretto.guilherme@gmail.com>
2024-01-31 10:55:06 +0000

    apply greyscale filter to avatar only on dark mode

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,20 +1,26 @@
 import styled from "styled-components";
+import { Theme, ThemeContextType } from "../../types/theme";
+import { useContext } from "react";
+import { DARK_THEME } from "../../themes/dark.theme";
+import { ThemeContext } from "../../contexts/ThemeContext";
+
 
 export interface IAvatarProps {
 	src: string;
 	alt?: string;
 }
 
-const AvatarImg = styled.img`
+const AvatarImg = styled.img<{theme: Theme}>`
 	overflow: hidden;
 	width: 130px;
 	border-radius: 50%;
-	filter: grayscale(100%);
+	filter: ${props => props.theme === DARK_THEME ? 'grayscale(100%)' : 'none'};
 	-webkit-box-shadow: 8px 11px 31px -19px rgba(0, 0, 0, 0.75);
 	-moz-box-shadow: 8px 11px 31px -19px rgba(0, 0, 0, 0.75);
 	box-shadow: 8px 11px 31px -19px rgba(0, 0, 0, 0.75);
 `;
 
 export default function Avatar({ src, alt }: IAvatarProps) {
-	return <AvatarImg src={src} alt={alt ?? ""} />;
+	const { theme } = useContext(ThemeContext) as ThemeContextType;
+	return <AvatarImg theme={theme} src={src} alt={alt ?? ""} />;
 }
